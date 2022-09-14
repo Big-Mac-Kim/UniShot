3f7d48f8008bd38f93ee850811f8e800d0468a6a
ntutindustry40 <industry4.0b014@gmail.com>
2022-09-14 22:59:55 +0800

    upload

--- unishot_cam_DEPTH.py
+++ unishot_cam_DEPTH.py
@@ -1,17 +1,15 @@
 import cv2
 import numpy as np
 from tensorflow.compat.v1.keras.models import load_model
-import time
 
 
 cap = cv2.VideoCapture(0)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
+MODEL_FILENAME= "depth_13.hdf5"
 
-MODEL_FILENAME= "D:/oneshot/master_D.hdf5"
-
-model = load_model(MODEL_FILENAME)# 自動建立目錄
+model = load_model(MODEL_FILENAME)
 print("model loaded")
 
 frame=np.zeros((480,640,3),dtype=int)
@@ -35,52 +33,36 @@ frame=frame[0:asize[0],b:b+asize[0]]
 
 frame=cv2.resize(frame, (400, 400), interpolation=cv2.INTER_AREA)
 frame=frame[100:300,100:300,:]
-cv2.imshow("s",frame)
+cv2.imshow("BASE_FRAME",frame)
 base_frame=cv2.resize(frame, (64, 64), interpolation=cv2.INTER_AREA)
 base_frame=base_frame/255.0 
-
-
-
 base_frame = np.expand_dims(base_frame, axis=0)
 
-a=0
-while True:
-    
-    if a==0:    
-        start_time=time.time()
-        
+
+while True:       
     ret, frame1 = cap.read()  
     asize=frame1.shape
     b=int((asize[1]-asize[0])/2)
     frame1=frame1[0:asize[0],b:b+asize[0]]
     
-  #  zommed_frame=frame1#[140:140+200,140:140+200]
+  
     zommed_frame=cv2.resize(frame1, (400, 400), interpolation=cv2.INTER_AREA)
     zommed_frame= zommed_frame[100:300,100:300,:]
     
     
-    cv2.imshow("ss",  zommed_frame)    
+    cv2.imshow("CAM LOOKING",  zommed_frame)    
     zommed_frame=cv2.resize(zommed_frame, (64, 64), interpolation=cv2.INTER_AREA)
     
     zommed_frame=zommed_frame/255.0    
   
     zommed_frame= np.expand_dims(zommed_frame, axis=0)  
-   
-    
+       
     Predicted_Probability = model.predict( [ zommed_frame,base_frame])
     X=np.argmax(Predicted_Probability,axis=1)
     X=int(X)
     print("X:"+str(X)) 
 
-    a=a+1
-    if a==30:
-        end_time=time.time()
-        run_time=end_time-start_time
-        print("FPS:"+str(30/run_time))
-        a=0
-    
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 cap.release()
 cv2.destroyAllWindows()
-
